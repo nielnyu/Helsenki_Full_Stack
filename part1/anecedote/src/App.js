1371941ec5527a8574b05179db4f364ffbaa1a61
nielnyu <ndc338@nyu.edu>
2023-06-29 22:25:35 -0400

    Annecodote question

---
+++ part1/anecedote/src/App.js
@@ -0,0 +1,69 @@
+import { useState } from 'react'
+import Text from './Text';
+import Button from './Button'
+
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+
+  const [selected, setSelected] = useState(0)
+  const [vote, setVote] = useState(new Uint8Array(8))
+
+  const handleClick = () =>{
+    setSelected(Math.floor(Math.random() * anecdotes.length))
+  }
+
+
+  const handleVote = (e) =>{
+    const copy = [...vote]
+    copy[selected] += 1
+    setVote(copy)
+  }
+
+  const max_vote = Math.max(...vote)
+
+
+  if (max_vote === 0){
+    return(
+      <div>
+        <h1>Anecdote of the Day!</h1>
+        <Text text = {anecdotes[selected]}/>
+        <Button handleClick = {handleVote} title = {"Vote"}/>
+        <Button handleClick = {handleClick} title  = {"Next Anecdote"}/>
+        <Text text = {"has " + vote[selected]  + " votes"}/> 
+      </div>
+    )
+  }
+  else{
+    const index = vote.indexOf(max_vote)
+    return(
+      <div>
+        <h1>Anecdote of the Day!</h1>
+        <Text text = {anecdotes[selected]}/>
+        <Button handleClick = {handleVote} title = {"Vote"}/>
+        <Button handleClick = {handleClick} title  = {"Next Anecdote"}/>
+        <Text text = {"has " + vote[selected]  + " votes"}/> 
+        <h1>Anecdote with the most votes</h1>
+        <Text text = {anecdotes[index]}/>
+        <Text text = {"has " + max_vote  + " votes"}/> 
+
+      </div>
+    )
+
+  }
+
+
+
+  
+
+}
+
+export default App;
---
+++ part1/anecedote/src/Button.js
@@ -0,0 +1,9 @@
+const Button = (props) =>{
+    return(
+        <div>
+            <button onClick = {props.handleClick}>{props.title}</button>
+        </div>
+    )
+}
+
+export default Button
