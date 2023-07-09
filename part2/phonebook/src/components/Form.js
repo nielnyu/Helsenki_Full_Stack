dfd55e4d4f23ecc01adeb7574d2092071d7ef48f
nielnyu <ndc338@nyu.edu>
2023-07-09 01:38:15 -0400

    Added filtering and alert

--- part2/phonebook/src/components/Form.js
+++ part2/phonebook/src/components/Form.js
@@ -1,11 +1,6 @@
 const Form = (props) =>{
     return(
-        <div>
-            <form onSubmit = {props.onSubmit}>
-                {props.subject} : <input placeholder= {props.placeholder} onChange = {props.onChange}/>
-                <button type = "submit">{props.buttonName}</button>
-            </form>
-        </div>
+        <form onSubmit={props.onSubmit}></form>
     )
 }
 
